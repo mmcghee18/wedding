9d31b12126b4ff76befd12ca2bacc1f790948ff2
Michelle McGhee <michmac202@gmail.com>
2024-01-29 13:25:56 -0800

    update github pages

---
+++ docs/_app/immutable/chunks/paths.ed8c6fd2.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_16j9gca)==null?void 0:s.base)??"/wedding";var a;const t=((a=globalThis.__sveltekit_16j9gca)==null?void 0:a.assets)??e;export{t as a,e as b};
--- docs/_app/immutable/chunks/paths.f2dfee45.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_7rkt26)==null?void 0:s.base)??"/wedding";var t;const a=((t=globalThis.__sveltekit_7rkt26)==null?void 0:t.assets)??e;export{a,e as b};
