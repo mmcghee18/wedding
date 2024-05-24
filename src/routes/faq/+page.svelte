0d2f3bbf56ce758998da4db4ea34fb59f3c2a6db
Michelle McGhee <michmac202@gmail.com>
2024-05-23 21:11:13 -0700

    indoors or outdoors

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -5,8 +5,9 @@
 
 	const faqs = [
 		{
-			question: "What's the weather going to be like?",
-			answer: "Low 70s during the day and mid-50s at night. Bring a layer!"
+			question:
+				"Where do I park for the ceremony and reception at Waterfall Lodge?",
+			answer: `If you're staying at Waterfall Lodge, you'll be able to park next to your cabin. If not, you'll take a short shuttle ride in. See the <a href='${base}/travel'>travel page</a> for more details.`
 		},
 		{
 			question: "Is there a dress code for Saturday?",
@@ -14,9 +15,13 @@
 				"<a href=https://www.theknot.com/content/what-to-wear-semi-formal#what-is-semi-formal-wedding target=_blank>Semi-formal</a> or (a term we made up) Forest Formal!\n\nBy which we mean, something special and fancy, but also reasonable to wear in the woods. We'd recommend wearing shoes that are comfortable to walk in."
 		},
 		{
-			question:
-				"Where do I park for the ceremony and reception at Waterfall Lodge?",
-			answer: `If you're staying at Waterfall Lodge, you'll be able to park next to your cabin. If not, you'll take a short shuttle ride in. See the <a href='${base}/travel'>travel page</a> for more details.`
+			question: "What's the weather going to be like?",
+			answer: "Low 70s during the day and mid-50s at night. Bring a layer!"
+		},
+		{
+			question: "Are the ceremony and reception indoors or outdoors?",
+			answer:
+				"The ceremony, cocktail hour, and dinner will be held outdoors in the redwoods. The dance floor is indoors. We'll have heaters and blankets available if it's chilly."
 		}
 	];
 	``;
